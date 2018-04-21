9d172c89afc5953e99e57fa0b2ec70b43f10e83d
ptruffault <ptruffault@hotmail.com>
2018-04-21 22:50:57 +0200

    make save

--- libft/ft_get_input.c
+++ libft/ft_get_input.c
@@ -1,20 +1,29 @@
 #include "libft.h"
 
-
-//add static char **
-//complétion ?
-// gestion touches spéciales
-
-
 char *ft_get_input(void)
 {
 	char *input;
+	int		ret;
+	char	buf;
+	int		i;
+	int		count;
 
-	if (get_next_line(0, &input) == -1)
+	if(!(input = ft_strnew(1)))
+		return (NULL);
+	count = 1;
+	i = 0;
+	while ((ret = read(0, &buf, 1)) && buf != '\n')
 	{
-		if (input)
-			free(input);
+		input[i++] = buf;
+		input = ft_realloc(input, count, count + 1);
+		count++;
+	}
+	input[i] = '\0';
+	if (!ret)
+	{
+		free(input);
 		return (NULL);
 	}
-	return(input);
+	return (input);
 }
+	
